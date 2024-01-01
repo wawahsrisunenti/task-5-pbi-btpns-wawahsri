8fad8fc62fef0b90bfe0f54ff2255f76b378bf5d
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2024-01-01 20:26:42 +0700

    feat (app): photo.go

--- app/photo.go
+++ app/photo.go
@@ -5,17 +5,18 @@ import (
 	"time"
 )
 
-// Photo struct represents the data model for a photo.
-type Photo struct {
-	ID        UUIDString `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Title     string     `gorm:"size:255;not null;" json:"title"`
-	Caption   string     `gorm:"not null;" json:"caption"`
-	PhotoUrl  string     `gorm:"not null;" json:"photo_url"`
-	UserID    UUIDString `gorm:"not null" json:"user_id"`
-	CreatedAt time.Time  `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt time.Time  `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+// CustomPhoto struct represents the data model for a photo with modified field names.
+type CustomPhoto struct {
+	ID        UUIDString `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"custom_id"`
+	Title     string     `gorm:"size:255;not null;" json:"custom_title"`
+	Caption   string     `gorm:"not null;" json:"custom_caption"`
+	PhotoURL  string     `gorm:"not null;" json:"custom_photo_url"`
+	UserID    UUIDString `gorm:"not null" json:"custom_user_id"`
+	CreatedAt time.Time  `gorm:"default:CURRENT_TIMESTAMP" json:"custom_created_at"`
+	UpdatedAt time.Time  `gorm:"default:CURRENT_TIMESTAMP" json:"custom_updated_at"`
 }
 
+// UUIDString is a custom type for UUID representation as string.
 type UUIDString string
 
 func (u UUIDString) Value() (driver.Value, error) {
